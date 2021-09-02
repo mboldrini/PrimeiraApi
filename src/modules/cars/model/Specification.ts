ae7a50caf60b87e66817ddd599486229dd5f7bf1
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-09-02 19:10:42 -0300

    separando tudo por módulos

--- src/modules/cars/model/Specification.ts
+++ src/modules/cars/model/Specification.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidV4 } from 'uuid';
-
-class Category {
+// model = entities
+class Specification{
     id?: string;
     name: string;
     description: string;
@@ -12,4 +12,4 @@ class Category {
         }
     }
 }
-export {Category}
+export {Specification}
