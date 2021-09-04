92df7a4928b33c27583e4a7df0727fbaf70b0001
Matheus Boldrini <matheus.souza.boldrini@hotmail.com>
2021-09-04 18:04:46 -0300

    começando a API do zero recomeçando a api novamente, anotando conceitos no caderno, e prestando muito mais atenção nos detalhes

--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
-import { ListCategoriesController } from "./listCategoriesController";
-import { ListCategoriesUseCase } from "./listCategoriesUseCase";
-
-const categoriesRepository = new CategoriesRepository();
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
-const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
-
-export { listCategoriesController };
